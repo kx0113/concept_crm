5ffa51230b58da758d52aabaa3ed1682b8952a7f
xushenkai <xushenkai@classba.cn>
2018-12-18 01:29:40 +0800

    test

--- backend/views/orders/view.php
+++ backend/views/orders/view.php
@@ -55,22 +55,26 @@ $this->params['breadcrumbs'][] = $this->title;
                                     <td class="tabletrtdleft tabletrtdbold"  colspan="8">施工费：<?php if(isset($orders_info['info']['work_cost'])){ echo $orders_info['info']['work_cost']; }?></td>
                                 </tr>
                                 <tr>
-                                    <td class="tabletrtdbold" colspan="1">序号</td>
-                                    <td class="tabletrtdbold"  colspan="1">产品名称</td>
-                                    <td class="tabletrtdbold"  colspan="1">编号</td>
-                                    <td class="tabletrtdbold"  colspan="1">品牌分类</td>
-                                    <td class="tabletrtdbold"  colspan="1">规格</td>
-                                    <td class="tabletrtdbold"  colspan="1">物品分类</td>
-                                    <td class="tabletrtdbold"  colspan="1">单位</td>
-                                    <td class="tabletrtdbold"  colspan="1">零售价</td>
-                                    <td class="tabletrtdbold"  colspan="1">成本价</td>
-                                    <td class="tabletrtdbold"  colspan="1">实际数量</td>
-                                    <td class="tabletrtdbold"  colspan="1">归还数量</td>
-                                    <td class="tabletrtdbold"  colspan="1">总用数量</td>
-                                    <td class="tabletrtdbold"  colspan="1">零售总价</td>
-                                    <td class="tabletrtdbold"  colspan="1">成本总价</td>
-                                    <td class="tabletrtdbold"  colspan="1">成本总合计</td>
-                                    <td class="tabletrtdbold"  colspan="1">操作</td>
+                                    <td class="tabletrtdleft tabletrtdbold"  colspan="8">成本总价：<?php echo $orders_info['stock_sum']['total_purchase_price']; ?></td>
+                                    <td class="tabletrtdleft tabletrtdbold"  colspan="8">市场总价：<?php echo $orders_info['stock_sum']['total_market_price']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td class="tabletrtdbold" width="50" colspan="1">序号</td>
+                                    <td class="tabletrtdbold" width="150 colspan="1">产品名称</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">编号</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">品牌分类</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">规格</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">物品分类</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">单位</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">零售价</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">成本价</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">实际数量</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">归还数量</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">总用数量</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">零售总价</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">成本总价</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">差价总价</td>
+                                    <td class="tabletrtdbold" width="100" colspan="1">操作</td>
 
                                 </tr>
                                 <?php if(isset($orders_info['stock_logs']) && !empty($orders_info['stock_logs'])){
@@ -91,7 +95,7 @@ $this->params['breadcrumbs'][] = $this->title;
                                                  <td  colspan="1"><?php echo $orders_info['stock_logs'][$k1]['current_number']; ?></td>
                                                  <td  colspan="1">¥<?php echo $orders_info['stock_logs'][$k1]['row_market_price']; ?></td>
                                                  <td  colspan="1">¥<?php echo $orders_info['stock_logs'][$k1]['row_purchase_price']; ?></td>
-                                                 <td  colspan="1">¥<?php echo $orders_info['stock_logs'][$k1]['row_purchase_price']; ?></td>
+                                                 <td  colspan="1">¥<?php echo bcsub($orders_info['stock_logs'][$k1]['row_market_price'],$orders_info['stock_logs'][$k1]['row_purchase_price'],2); ?></td>
 <!--                                                 <td  rowspan="--><?php //echo $orders_info['stock_sum']['total_data_count']; ?><!--">-->
 <!--                                                     --><?php //echo $orders_info['stock_sum']['total_purchase_price']; ?>
 <!--                                                 </td>-->
@@ -100,87 +104,20 @@ $this->params['breadcrumbs'][] = $this->title;
                                              </tr>
                                 <?php  }} ?>
 
-
-
-
-
-
                             </table>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6" style="display: none;">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <h5>订单基础信息</h5>
-                            <div class="ibox-tools">
-
+                            <div style="float: right">
+                                <button type="button" class="btn btn-warning">发送Email</button>
+                                <button type="button" class="btn btn-primary">批量归还</button>
+                                <button type="button" class="btn btn-success">批量出库</button>
+                                <button type="button" class="btn btn-info">导出Excel</button>
+                                <button type="button" class="btn btn-danger">打印订单</button>
                             </div>
-                        </div>
-                        <div class="ibox-content">
-                            <p>名称：<?php if(isset($orders_info['info']['name'])){ echo $orders_info['info']['name']; }?></p>
-                            <p>地址：<?php if(isset($orders_info['info']['address'])){ echo $orders_info['info']['address']; }?></p>
-                            <p>合同开始日期：<?php if(isset($orders_info['info']['start_time'])){ echo date("Y-m-d",strtotime($orders_info['info']['start_time'])); }?></p>
-                            <p>合同结束日期：<?php if(isset($orders_info['info']['end_time'])){ echo date("Y-m-d",strtotime($orders_info['info']['end_time'])); }?></p>
-                            <p>联系电话：<?php if(isset($orders_info['info']['phone'])){ echo $orders_info['info']['phone']; }?></p>
-                            <p>施工费用：<?php if(isset($orders_info['info']['work_cost'])){ echo $orders_info['info']['work_cost']; }?></p>
-                            <p>运费：<?php if(isset($orders_info['info']['freight_cost'])){ echo $orders_info['info']['freight_cost']; }?></p>
-                            <p>备注：<?php if(isset($orders_info['info']['remark'])){ echo $orders_info['info']['remark']; }?></p>
-                            <p>状态：<?php if(isset($orders_info['info']['status'])){ echo \common\models\Orders::get_status($orders_info['info']['status']); }?></p>
-                            <p>公司名称：<?php if(isset($orders_info['info']['token'])){ echo \common\models\Web::GetWebName($orders_info['info']['token']); }?></p>
-                            <p>创建用户：<?php if(isset($orders_info['info']['add_user'])){ echo  \common\models\User::get_username($orders_info['info']['add_user']); }?></p>
-                            <p>更新时间：<?php if(isset($orders_info['info']['update_at'])){ echo $orders_info['info']['update_at']; }?></p>
-                            <p>创建时间：<?php if(isset($orders_info['info']['create_at'])){ echo $orders_info['info']['create_at']; }?></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6" style="display: none;">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <h5>客户基础信息</h5>
-                            <div class="ibox-tools">
-
-                            </div>
-                        </div>
-                        <div class="ibox-content">
-                            <p>名称：<?php if(isset($orders_info['customer_info']['name'])){ echo $orders_info['customer_info']['name']; }?></p>
-                            <p>电话：<?php if(isset($orders_info['customer_info']['phone'])){ echo $orders_info['customer_info']['phone']; }?></p>
-                            <p>地址：<?php if(isset($orders_info['customer_info']['address'])){ echo $orders_info['customer_info']['address']; }?></p>
-                            <p>客户类型：<?php if(isset($orders_info['customer_info']['customer_type'])){ echo \common\models\Types::getName($orders_info['customer_info']['customer_type']); }?></p>
-                            <p>客户来源：<?php if(isset($orders_info['customer_info']['source_type'])){ echo \common\models\Types::getName($orders_info['customer_info']['source_type']); }?></p>
-                            <p>客户状态：<?php if(isset($orders_info['customer_info']['status'])){ echo \common\models\Types::getName($orders_info['customer_info']['status']); }?></p>
-                            <p>备注：<?php if(isset($orders_info['customer_info']['remark'])){ echo $orders_info['customer_info']['remark']; }?></p>
-                            <p>公司名称：<?php if(isset($orders_info['customer_info']['token'])){ echo \common\models\Web::GetWebName($orders_info['customer_info']['token']); }?></p>
-                            <p>创建用户：<?php if(isset($orders_info['customer_info']['add_user'])){ echo \common\models\User::get_username($orders_info['customer_info']['add_user']); }?></p>
-                            <p>更新时间：<?php if(isset($orders_info['customer_info']['update_at'])){ echo $orders_info['customer_info']['update_at']; }?></p>
-                            <p>创建时间：<?php if(isset($orders_info['customer_info']['create_at'])){ echo $orders_info['customer_info']['create_at']; }?></p>
-                            <br>
-                            <br>
-                            <br>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row" style="display: none;">
-                <div class="col-sm-12">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <h5>出入库信息</h5>
-                            <div class="ibox-tools">
-
-                            </div>
-                        </div>
-                        <div class="ibox-content">
-
-                            <p>公司名称：123456</p>
-                            <p>创建用户：123456</p>
-                            <p>更新时间：123456</p>
-                            <p>创建时间：123456</p>
+                            <div style="clear: both;"></div>
                         </div>
                     </div>
                 </div>
 
-            </div>
+
         </div>
 
 </div>
